7ae9a9f5bcdca7580efb9c21d1d0dccdcd729a38
RaihanDaffa <30395700+RaihanDaffa@users.noreply.github.com>
2017-10-16 07:24:33 +0700

    Add files via upload

---
+++ XI-RPL2-28/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="form">
+	<div class="content">
+		<h1>Instagram</h1>
+		<h3>Sign up to see photos and videos<br>from your fiends</h3>
+		<a href="www.facebook.com"><button>Login with Facebook</button><br></a>
+		<p style="margin: 15px; color:black">OR</p>
+		<form method="POST" action="done.php">
+		<input type="text" name="email" placeholder="Mobile Number or Email"><br>
+		<input type="text" name="full-name" placeholder="Full Name"><br>
+		<input type="text" name="username" placeholder="Username"><br>
+		<input type="password" name="password" placeholder="Password"><br>
+		<button type="submit">Sign Up</button><br>
+		<p>By signing up, you agree to our<br><b>Terms & Privacy Policy</b>.</p><br>
+		<h4>Have an account?<a href=#>Log in</button></h4>
+		</form>
+	</div>
+</body>
+</html>
