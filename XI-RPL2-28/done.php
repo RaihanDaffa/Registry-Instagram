3d1c8441a46d8048fb42bcbdb7c62326259a5d04
RaihanDaffa <30395700+RaihanDaffa@users.noreply.github.com>
2017-10-23 05:31:48 +0700

    Add files via upload

--- XI-RPL2-28/done.php
+++ XI-RPL2-28/done.php
@@ -1,30 +1,74 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Registrasi Sukses</title>
+	<meta charset="UTF-8">
+	<title>Hasil</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 <p align="center"><font size="5px"><b>Registrasi Sukses</b></font></p>
 <table align="center">
-<tr>
-<td>Mobile Number or Email</td><td>:</td><td><?php echo $_POST["email"];?>
-</td>
-</tr>
+<?php 
 
-<tr>
-<td>Full Name</td><td>:</td><td><?php echo $_POST["full-name"];?>
-</td>
-</tr>
+$host= "localhost";
+$user= "root";
+$password= "";
+$email=$_POST['email'];
+$fullname=$_POST['full-name'];
+$username=$_POST['username'];
+$password=$_POST['password'];
 
-<tr>
-<td>Username</td><td>:</td><td><?php echo $_POST["username"];?>
-</td>
-</tr>
 
-<tr>
-<td>Password</td><td>:</td><td><?php echo $_POST["password"];?>
-</td>
-</tr>
+if($username)
+{
+$conn=mysqli_connect ($host,$user,$password);
+mysqli_select_db ($conn,'dbinstagram');
+$sql="insert into tbinstagram values ('$email', '$fullname', '$username', '$password')";
+$hasil=mysqli_query ($conn,$sql); 
+}
+
+else{
+	echo "Data Gagal Disimpan";
+}
+
+
+?>
+
+<div id="container">
+	<div class="Header">
+		Verivication
+	</div>
+
+	<div class="hasil">
+		<div class="email">
+			Email or Number Phone: <?php echo" $email" ?><br>
+		</div>
+
+		<div class="fullname">
+			Full name : <?php echo "$fullname"; ?><br>
+		</div>
+
+		<div class="user">
+			Username  : <?php echo "$username"; ?><br>
+		</div>
+
+		<div class="password">
+			Password : <?php echo "$password"; ?><br>
+		</div>
+		
+		
+	</div>
+</div>
+
+
+
+
+
+
+
 </table>
 </body>
 </html>
+
+
+
